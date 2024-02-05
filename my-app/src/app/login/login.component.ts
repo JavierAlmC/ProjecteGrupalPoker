4559e0b088a268c2b7cb547837af99dd12361128
Faristol <joseptortosa33@gmail.com>
2024-02-05 11:22:42 +0100

    login registre estructura inicial

---
+++ my-app/src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatFormFieldModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  
+
+
+
+}
