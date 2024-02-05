5f30d39c23a8ed6f1672814cd564d2ad27ee1755
Faristol <joseptortosa33@gmail.com>
2024-02-05 12:10:28 +0100

    loginnnnnn materiaal especial

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 export const routes: Routes = [
-    {path:'', component:LoginComponent},
+    {path:'**', component:LoginComponent},
     {path:'register', component:RegistrationComponent}
 
 ];
--- my-app/src/app/components/main/main.component.ts
+++ my-app/src/app/components/main/main.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { LoginComponent } from '../login/login.component';
-import { RegistrationComponent } from '../registration/registration.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [LoginComponent,RegistrationComponent,RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
