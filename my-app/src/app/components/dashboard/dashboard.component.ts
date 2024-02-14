ae866df2da7df88e2b548b8bd2595ba3a51f7bfb
sergi Blasco <sblascogimenez@gmail.com>
2024-02-14 21:24:36 +0100

    Dashboard acabat

--- my-app/src/app/components/dashboard/dashboard.component.ts
+++ my-app/src/app/components/dashboard/dashboard.component.ts
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  money: number = 10000;
+  initial_money: number = 1000;
+  total_bet = 32000;
 
+  calcularRendimiento(): number {
+    return ((this.money - this.initial_money) / this.initial_money) * 100;
+  }
 }
