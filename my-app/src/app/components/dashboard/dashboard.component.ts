ab97db5d6763646438b0fc0e19ff342584512599
sergi Blasco <sblascogimenez@gmail.com>
2024-02-19 09:40:04 +0100

    no fer cas

--- my-app/src/app/components/dashboard/dashboard.component.ts
+++ my-app/src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { MatchesService } from '../../services/matches.service';
+import { UserServicesService } from '../../services/user-services.service';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -7,10 +8,29 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
-  money: number = 10000;
+export class DashboardComponent implements OnInit {
+  money: number = 0; 
+  
+  ngOnInit(): void {
+    const nickname = this.userServicesService.getNickname() || '';
+    
+    this.matchesService.getUserMoney(nickname).subscribe(
+      (money: number) => {
+        this.money = money;
+      },
+      (error: any) => {
+        console.error('Error al obtener el dinero del usuario:', error);
+        
+      }
+    );
+  }
+
   initial_money: number = 1000;
-  total_bet = 32000;
+  total_bet = 0;
+
+  constructor(private matchesService: MatchesService, private userServicesService: UserServicesService) {
+    
+  }
 
   calcularRendimiento(): number {
     return ((this.money - this.initial_money) / this.initial_money) * 100;
