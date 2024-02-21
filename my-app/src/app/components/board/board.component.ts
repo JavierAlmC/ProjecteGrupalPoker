a41c54b43d01ecc36217365e06f01a66f79ed8df
sergi Blasco <sblascogimenez@gmail.com>
2024-02-21 15:17:39 +0100

    Acabat

--- my-app/src/app/components/board/board.component.ts
+++ my-app/src/app/components/board/board.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
-  selector: 'app-board',
-  standalone: true,
-  imports: [],
-  templateUrl: './board.component.html',
-  styleUrl: './board.component.css'
+    selector: 'app-board',
+    standalone: true,
+    templateUrl: './board.component.html',
+    styleUrl: './board.component.css',
+    imports: [HeaderComponent, FooterComponent]
 })
 export class BoardComponent {
 
